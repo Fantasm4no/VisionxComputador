49f1c578ce0ef40aa8495d0a0317678c30fc9af7
HenryGranda <huambi123@gmail.com>
2025-05-26 14:31:32 -0500

    Mensaje de prueba desde mi nueva rama

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <iostream>
+
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     MainWindow w;
     w.show();
     return app.exec();
+    std::cout << "Hola desde la rama jhosselyn-filtros" << std::endl;
+
 }
